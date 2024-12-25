87312a9a7ba491bef86750f6c5cd340fdd30bcc6
Tom Rowsell <rowselltom@gmail.com>
2024-12-25 21:25:47 +0000

    Day6 - done at last. brute force DFS on the second part is slooooow

--- Day06/main.py
+++ Day06/main.py
@@ -1,10 +1,9 @@
 from typing import Tuple
-from collections import deque
 
 directionSet = set(["<", ">", "^", "v"])
 
 
-def parseLine(linePlusIndex: Tuple[int, str]):
+def parse_line(linePlusIndex: Tuple[int, str]):
     y, line = linePlusIndex
     return list(
         map(
@@ -17,122 +16,100 @@ def parseLine(linePlusIndex: Tuple[int, str]):
     )
 
 
-def tracePath(position: Tuple[int, int], obstacles: list[Tuple[int, int]]):
-    x, y = position
-    return any([(ox, oy) for ox, oy in obstacles if ox == x and oy == y])
-
-
-def moveGuard(
-    guardPosition: Tuple[int, int, str],
+def move_guard(
+    guard_position: Tuple[int, int, str],
     obstacles: list[Tuple[int, int]],
     visited: set[Tuple[int, int]],
     dimensions: Tuple[int, int],
-    lastEightVertices: deque[Tuple[int, int]] = deque(maxlen=8),
+    guard_vertices: set[Tuple[int, int, str]] = set(),
 ):
     # abandon the hash method as cycles could be any multiple of 4
     # instead record a list of guard positions instead and for every move check against the start of the record list and tick off each consecutive vertex
     #
-    print(lastEightVertices)
-    primeFactors = [13, 5, 7, 11]
-    if len(lastEightVertices) == 8:
-        hashValFirstFour = sum(
-            [p * x * y for p, (x, y) in zip(primeFactors, lastEightVertices)]
-        )
-        hashValLastFour = sum(
-            [p * x * y for p, (x, y) in zip(primeFactors, list(lastEightVertices)[-4:])]
-        )
-        print("hash of first four: %s %s" % (hashValFirstFour, hashValLastFour))
-        if hashValFirstFour == hashValLastFour:
-            print("matched!!!!!!!!!!!!!!")
-            return visited, True
-
-    sizeX, sizeY = dimensions
-    match guardPosition:
+    size_x, size_y = dimensions
+    match guard_position:
         case (x, y, "^"):
             ys = [oy for (ox, oy) in obstacles if ox == x and oy < y]
             if len(ys) == 0:
                 visited = visited.union(map(lambda vy: (x, vy), range(y, -1, -1)))
                 return visited, False
-            nextYObstacle = max(ys)
+            next_y_obstacle = max(ys)
             visited = visited.union(
-                map(lambda yb: (x, yb), range(y, nextYObstacle, -1))
+                map(lambda yb: (x, yb), range(y, next_y_obstacle, -1))
             )
-            guardPosition = (x, nextYObstacle + 1, ">")
+            guard_position = (x, next_y_obstacle + 1, ">")
         case (x, y, ">"):
             xs = [ox for ox, oy in obstacles if oy == y and ox > x]
             if len(xs) == 0:
-                visited = visited.union(map(lambda vx: (vx, y), range(x, sizeX)))
+                visited = visited.union(map(lambda vx: (vx, y), range(x, size_x)))
                 return visited, False
-            nextXObstacle = min(xs)
-            visited = visited.union(map(lambda xb: (xb, y), range(x, nextXObstacle)))
-            guardPosition = (nextXObstacle - 1, y, "v")
+            next_x_obstacle = min(xs)
+            visited = visited.union(map(lambda xb: (xb, y), range(x, next_x_obstacle)))
+            guard_position = (next_x_obstacle - 1, y, "v")
 
         case (x, y, "v"):
             ys = [oy for ox, oy in obstacles if ox == x and oy > y]
             if len(ys) == 0:
-                visited = visited.union(map(lambda vy: (x, vy), range(y, sizeY)))
+                visited = visited.union(map(lambda vy: (x, vy), range(y, size_y)))
                 return visited, False
-            nextYObstacle = min(ys)
-            visited = visited.union(map(lambda yb: (x, yb), range(y, nextYObstacle)))
-            guardPosition = (x, nextYObstacle - 1, "<")
+            next_y_obstacle = min(ys)
+            visited = visited.union(map(lambda yb: (x, yb), range(y, next_y_obstacle)))
+            guard_position = (x, next_y_obstacle - 1, "<")
         case (x, y, "<"):
             xs = [ox for ox, oy in obstacles if oy == y and ox < x]
             if len(xs) == 0:
                 visited = visited.union(map(lambda vx: (vx, y), range(x, -1, -1)))
                 return visited, False
-            nextXObstacle = max(xs)
+            next_x_obstacle = max(xs)
             visited = visited.union(
-                map(lambda xb: (xb, y), range(x, nextXObstacle, -1))
+                map(lambda xb: (xb, y), range(x, next_x_obstacle, -1))
             )
-            guardPosition = (nextXObstacle + 1, y, "^")
-    lastEightVertices.append((guardPosition[0], guardPosition[1]))
-    return moveGuard(
-        guardPosition,
+            guard_position = (next_x_obstacle + 1, y, "^")
+
+    if guard_position in guard_vertices:
+        return visited, True
+    guard_vertices = guard_vertices.union([guard_position])
+    return move_guard(
+        guard_position,
         obstacles,
         visited,
         dimensions,
-        lastEightVertices=lastEightVertices,
+        guard_vertices=guard_vertices,
     )
 
 
-def partB(visited, obstacles, guardPosition, dimensions):
+def partB(visited, obstacles, guard_position, dimensions):
     loopCounter = 0
-    dq = deque(maxlen=8)
     for vx, vy in visited:
-        print("%s %s" % (vx, vy))
         newObstacles = []
         newObstacles.extend(obstacles)
         newObstacles.append((vx, vy))
-        (_, isLoop) = moveGuard(
-            guardPosition, newObstacles, set(), dimensions, lastEightVertices=dq
-        )
+        (_, isLoop) = move_guard(guard_position, newObstacles, set(), dimensions)
         if isLoop:
-            loopCounter = loopCounter + 1
-        dq.clear()
+            loopCounter += 1
     return loopCounter
 
 
-gMap = []
-startPoint = None
+g_map = []
+start_point = None
 with open("input.txt") as file:
     lines = list(enumerate(file.readlines()))
 
-    sizeY = max([y + 1 for y, _ in lines])
-    sizeX = len(lines[0][1]) - 1
-    for line in filter(lambda a: any(a), map(parseLine, lines)):
+    size_y = max([y + 1 for y, _ in lines])
+    size_x = len(lines[0][1]) - 1
+    for line in filter(lambda a: any(a), map(parse_line, lines)):
         items = [(x, y, item) for x, y, item in line]
-        dirPoints = list(filter(lambda xyv: xyv[2] in directionSet, items))
-        if any(dirPoints):
-            [startPoint] = dirPoints
-        gMap.extend(items)
+        dir_points = list(filter(lambda xyv: xyv[2] in directionSet, items))
+        if any(dir_points):
+            [start_point] = dir_points
+        g_map.extend(items)
 
-if startPoint is None:
+if start_point is None:
     raise Exception("no start point found")
 
-obstacles = [(x, y) for x, y, v in gMap if v not in directionSet]
-sx, sy, _ = startPoint
-(results, _) = moveGuard(startPoint, obstacles, set([(sx, sy)]), (sizeX, sizeY))
-print("partB starting ==============")
-partBResult = partB(results, obstacles, startPoint, (sizeX, sizeY))
-print(partBResult)
+obstacles = [(x, y) for x, y, v in g_map if v not in directionSet]
+sx, sy, _ = start_point
+(results, _) = move_guard(start_point, obstacles, set([(sx, sy)]), (size_x, size_y))
 print(len(results))
+part_b_result = partB(results, obstacles, start_point, (size_x, size_y))
+print(part_b_result)
