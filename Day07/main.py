99530006852d5c61c9aec7c115fb7b197d6910b0
Tom Rowsell <rowselltom@gmail.com>
2024-12-28 11:49:51 +0000

    refactored day 7

--- Day07/main.py
+++ Day07/main.py
@@ -1,18 +1,17 @@
-from functools import partial
-from itertools import accumulate
+from functools import partial, reduce
 from typing import Tuple
 
 
 def parts_acc(target: int, part_b: bool, state: Tuple[list[int], bool], part: int):
     running_totals, _ = state
     newTotals = [
-        rt for rt in acc_generator(part_b, running_totals, part) if rt <= target
+        rt for rt in operation_generator(part_b, running_totals, part) if rt <= target
     ]
 
     return newTotals, any([a for a in newTotals if a == target])
 
 
-def acc_generator(part_b, running_totals, new_value):
+def operation_generator(part_b, running_totals, new_value):
     for total in running_totals:
         yield total + new_value
         yield total * new_value
@@ -24,14 +23,14 @@ with open("input.txt") as file:
     part_a_total, part_b_total = 0, 0
     while line := file.readline().removesuffix("\n").replace(":", ""):
         [target, *parts] = [int(p) for p in line.split(" ")]
-        [*_, (*_, part_a_matched)] = list(
-            accumulate(parts, partial(parts_acc, target, False), initial=([0], False))
+        (*_, part_a_matched) = list(
+            reduce(partial(parts_acc, target, False), parts, ([0], False))
         )
         if part_a_matched:
             part_a_total += target
 
-        [*_, (*_, part_b_matched)] = list(
-            accumulate(parts, partial(parts_acc, target, True), initial=([0], False))
+        (*_, part_b_matched) = list(
+            reduce(partial(parts_acc, target, True), parts, ([0], False))
         )
         if part_b_matched:
             part_b_total += target
